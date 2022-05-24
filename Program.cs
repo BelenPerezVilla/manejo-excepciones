481d4f62e5f5138aa328fff28d89f78bd4174f20
BelenPerezVilla <L21212579@tectijuana.edu.mx>
2022-05-24 11:50:40 -0700

    creacion del codigo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            try
+            {
+                Console.Write("Teclee un numero entero: ");
+                numero = int.Parse(Console.ReadLine());
+                resultado = numero * numero;
+                Console.WriteLine("El numero " + numero + "al cuadrado es: " + resultado);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("el formato no cumple los requisitos");
+            }
+            catch(OverflowException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
